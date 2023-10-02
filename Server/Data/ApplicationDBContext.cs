62cd64b7c77a6ef251f2fcfdf46e8dd73514d4c0
blancout <blancout@gmail.com>
2023-10-02 17:56:41 -0300

    Add project files.

---
+++ Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorLearner.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorLearner.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
